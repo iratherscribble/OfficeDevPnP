0f66fcd083c2f99ff26f2a3f7c601a685ad82560
jprell <jprell@rightpoint.com>
2015-03-24 16:33:25 -0500

    Added a way to get the current connection

---
+++ Solutions/PowerShell.Commands/Commands/Base/GetSPOConnection.cs
@@ -0,0 +1,15 @@
+﻿using System.Management.Automation;
+using OfficeDevPnP.PowerShell.CmdletHelpAttributes;
+
+namespace OfficeDevPnP.PowerShell.Commands.Base
+{
+    [Cmdlet(VerbsCommon.Get, "SPOConnection")]
+    [CmdletHelp("Returns the current in-memory connection", Category = "Base Cmdlets")]
+    public class GetSPOConnection : PSCmdlet
+    {
+        protected override void ProcessRecord()
+        {
+            WriteObject(SPOnlineConnection.CurrentConnection);
+        }
+    }
+}
